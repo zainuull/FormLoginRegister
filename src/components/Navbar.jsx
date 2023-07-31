4015e70e4bf06ba8a65c6a5cd398958973192f35
zainuull <thobazainul@gmail.com>
2023-07-31 12:43:32 +0700

    mengupdate navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 const Navbar = () => {
   return (
-    <div className="w-full h-[50px] bg-gray-200 p-10 flex justify-between">
+    <div className="w-full h-[50px] bg-gray-200 p-10 flex justify-between items-center">
       <h1>Hello World</h1>
-      <div>
+      <div className="flex justify-evenly items-center w-1/2">
         <p>Beranda</p>
         <p>About</p>
         <p>Contact</p>
       </div>
+      <button className="w-[100px] h-[50px] rounded-md bg-blue-200">Send</button>
     </div>
   );
 }
