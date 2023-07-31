7e4eca87925b178d49e39599bed4299fefd70ddc
zainuull <thobazainul@gmail.com>
2023-07-31 12:35:34 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar"
+
 function App() {
 
 
   return (
     <div className="min-h-screen bg-green-200">
-
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="w-full h-[50px] bg-gray-200 p-10 flex justify-between">
+      <h1>Hello World</h1>
+      <div>
+        <p>Beranda</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
